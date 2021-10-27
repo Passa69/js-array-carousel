4a95add2d03cf39014f80c5fdc8ace395790e217
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-27 18:18:55 +0200

    aggiunta ciclo js per aggiunta in html

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+// creare i contenuti delle macroaree utilizzando gli array
+const itemsCont = document.querySelector(".items");
+const thumbsCont = document.querySelector(".thumbs");
+
+let item = "";
+let thumbs = "";
+
+// ciclo di aggiunta 
+for (let i=0; i < items.length; i++) {
+    thumbs += 
+    `<div class="thumb">
+        <img src="${items[i]}" alt="${title[i]}">
+    </div>`;
+
+    item += 
+    `<div class="item active">
+        <img src="${items[i]}" alt="${title[i]}">
+        <div class="text">
+            <h3>${title[i]}</h3>
+            <p>${text[i]}.</p>
+        </div>
+    </div>`;
+}
+
+itemsCont.innerHTML = item;
+thumbsCont.innerHTML = thumbs;
